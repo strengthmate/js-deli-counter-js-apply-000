ca0155ac4af56a36e9fb5ace1b35214b684b84f3
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-11 14:08:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+//var dailyTotal = 0;
+
 function takeANumber(katzDeli, otherDeli) {
+  //dailyTotal++;
   katzDeli.push(otherDeli);
   return `Welcome, ${otherDeli}. You are number ${katzDeli.length} in line.`;
 }
